974c0fa0e2f56d7d36d5db3c9a5d55d9fa981f6d
Ana Music <ancka.music@gmail.com>
2014-12-21 22:15:16 +0100

    commit

--- ProjectAnaMusic.r
+++ ProjectAnaMusic.r
@@ -3,22 +3,21 @@ library(caret); library(gbm); library(survival); library(splines); library(plyr)
 set.seed(1)
 
 # Set directory
-dir = "C:/Users/domen/Documents/Coursera_MachineLearning/"
+dir = "local_directory"
 setwd(dir)
 training = read.csv("pml-training.csv")
+testMain = read.csv("pml-testing.csv")
 
-# zbrisemo vse stoplce, ki imajo kaksno vrednost = NA
 training <- training[,colSums(is.na(training)) == 0] 
 columns = colnames(training)
 
-# razdelimo na train in test matriki
 inTrain = createDataPartition(y=training$classe,p=0.75,list=FALSE)
 train = training[inTrain,]
 test = training[-inTrain,]
 
 # parameter setting 
 fitControl <- trainControl(method  = "repeatedcv",
-                           number  = 5,
+                           number  = 7,
                            repeats = 2)
 
 
@@ -32,44 +31,12 @@ modFit <- train(classe ~ yaw_belt+total_accel_belt+total_accel_arm+accel_belt_z+
 end.time <- Sys.time()
 (end.time - start.time)
 
-varImp(modFit, scale = FALSE) # variable importance http://topepo.github.io/caret/varimp.html
+varImp(modFit, scale = FALSE)
 outturn = predict(modFit,test)
 
-# stevilno pravilno napovedanih razredov
+# ratio
 length(which(outturn == test$classe))/nrow(test)
 
-
-
-
-
-
-
-
-
-
-dummies = dummyVars(classe ~ ., data=training)
-head(predict(dummies,newdata=training))
-
-
-
-# Correlations
-a = 45
-b = a+4
-featurePlot(x=training[,c(columns[a:b],"classe")], 
-            y=training$classe, plot="pairs")
-columns[a:b]
-
-# Density functuins
-for (i in 1:length(columns) ) {
-  qplot(x=training[,columns[i]],colour=classe,data=training,xlab=columns[i])
-  ggsave(file=paste(i,columns[i],"_point.png",sep=""))
-}
-
-#install.packages("rpart")
-#install.packages("e1071")
-#install.packages("rJava")
-#install.packages("RWeka")
-
-#library(rJava)
-library(RWeka)
-
+# prediction
+predictions = predict(modFit,testMain)
+(predictions)
